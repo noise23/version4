c234e01adf0cdbc6e78426b0f9488805e36baf5a
noise23 <noise23@users.noreply.github.com>
2023-08-23 08:59:03 +0300

    Compact serialization for scripts. Make CPubKey statically allocated.
    Compact serialization for scripts:
    Special serializers for script which detect common cases and encode
    them much more efficiently. 3 special cases are defined:
    * Pay to pubkey hash (encoded as 21 bytes)
    * Pay to script hash (encoded as 21 bytes)
    * Pay to pubkey starting with 0x02, 0x03 or 0x04 (encoded as 33 bytes)
    Other scripts up to 121 bytes require 1 byte + script length. Above
    that, scripts up to 16505 bytes require 2 bytes + script length.
    https://github.com/bitcoin/bitcoin/commit/69fc8047a9a96bcf5360f810c796049c27e16fcd
    
    Make CPubKey statically allocated.
    https://github.com/bitcoin/bitcoin/commit/5d891489ab7828ad8db15e85bb63e2f13f021a6a

--- src/main.h
+++ src/main.h
@@ -703,6 +703,21 @@ protected:
     const CTxOut& GetOutputFor(const CTxIn& input, const MapPrevTx& inputs) const;
 };
 
+/** wrapper for CTxOut that provides a more compact serialization */
+class CTxOutCompressor
+{
+private:
+    CTxOut &txout;
+public:
+    CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn) { }
+
+    IMPLEMENT_SERIALIZE(
+        READWRITE(VARINT(txout.nValue));
+        CScriptCompressor cscript(REF(txout.scriptPubKey));
+        READWRITE(cscript);
+    )
+};
+
 /** A transaction with a merkle branch linking it to the block chain. */
 class CMerkleTx : public CTransaction
 {
